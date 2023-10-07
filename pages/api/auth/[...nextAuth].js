99de681c38ddd115504c65241e8503e1fc963f8e
NeoDitjoe <ditjoeneo03@gmail.com>
2023-10-07 20:50:31 +0200

    deployment

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -9,41 +9,32 @@ export default NextAuth({
     session : {
         jwt: true
     },
-    secret: process.env.NEXTAUTH_SECRET,
     
 
-    providers: [
-    CredentialsProvider({
+providers: [
+  CredentialsProvider({
 
-        async authorize(credentials) {
-            
-            const client = await connectDatabase('authentication')
-            const userscollection = client.db().collection('users')
-            const user = await userscollection.findOne({ email: credentials.email})
-            
-            if (!user){
-                client.close()
-                throw new Error('User not found!')
-            }
-
-            const isValid = await verifyPassword(credentials.password, user.password)
-
-            if(!isValid){
-                client.close()
-                throw new Error('Could not log you in')
-            }
+    async authorize(credentials) {
+        
+        const client = await connectDatabase('authentication')
+        const userscollection = client.db().collection('users')
+        const user = await userscollection.findOne({ email: credentials.email})
+        
+        if (!user){
             client.close()
-            return { email: [user.email, user.username] }
+            throw new Error('User not found!')
         }
-    })
-    ],
 
-    pages: {
-        signIn: '/auth/signin',
-        signOut: '/auth/signout',
-        error: '/auth/error',
-        verifyRequest: '/auth/verify-request',
-        newUser: '/auth/new-user',
-      },
+        const isValid = await verifyPassword(credentials.password, user.password)
+
+        if(!isValid){
+            client.close()
+            throw new Error('Could not log you in')
+        }
+        client.close()
+        return { email: [user.email, user.username] }
+    }
+  })
+]
 
 })
