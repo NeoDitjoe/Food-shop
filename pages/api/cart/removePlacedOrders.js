c5f9a657a181c9d3f8981cd08b86f1a302eefaa4
NeoDitjoe <ditjoeneo03@gmail.com>
2023-10-23 06:49:38 +0200

    delete placed orders

--- pages/api/cart/removePlacedOrders.js
+++ pages/api/cart/removePlacedOrders.js
@@ -10,7 +10,7 @@ export default async function handler(req, res){
         try{
             const db = client.db('cart')
 
-            db.collection('placedOrders').deleteOne({user: username, item : item})
+            db.collection('placedOrders').deleteOne({customer: username, item : item})
             res.status(200).json({ message: 'removed item' })
 
         }catch(error){
