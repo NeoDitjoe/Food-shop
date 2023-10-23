080e58972c35d124d9e3227da871361165d8d77e
NeoDitjoe <ditjoeneo03@gmail.com>
2023-10-23 06:34:42 +0200

    style orders page

--- pages/dashboard2090BoBo/orders.js
+++ pages/dashboard2090BoBo/orders.js
@@ -1,6 +1,6 @@
 import Error from "@/components/Error/Error"
 import { getMenuList } from "@/database/Database"
-import { useEffect } from "react"
+import { BsFillTrashFill } from "react-icons/bs";
 import style from 'styles/dashboard.module.css'
 
 export default function Orders({placedOrders}){
@@ -19,12 +19,25 @@ export default function Orders({placedOrders}){
             {
                 placedOrders.map((order) => {
                     return (
-                        <div key={order.totalPrice+order.customer+order.item} className={style.order}>
-                            <br/>
-                            <hr/>
-                            <h4>{order.customer}</h4>
-                            <p>{order.item}</p>
-                            <p>R {order.totalPrice.toFixed(2)}</p>
+                        <div key={order.totalPrice+order.customer+order.item+new Date()} className={style.orders}>
+                            <div className={style.order}>
+                                <h4>{order.customer}</h4>
+                                <p>{order.item}</p>
+                                <p className={style.price}>R {order.totalPrice.toFixed(2)}</p>
+                            </div>
+
+                            <div className={style.bin} onClick={() => {
+                                    try{
+                                        removeItem({ username: order.customer , item: order.item})
+                                        console.log('removed order')
+
+                                    }catch(error){
+                                        console.log('attempt failed')
+                                    }
+                                    }}>
+                                    <BsFillTrashFill color='red' size={25} />
+                                    
+                            </div>
                         </div>
                     )
                 })
@@ -42,4 +55,14 @@ export async function getServerSideProps(){
             placedOrders
         }
     }
+}
+
+async function removeItem(item){
+    await fetch('/api/cart/removePlacedOrders', {
+        method: 'POST',
+        body: JSON.stringify(item),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
 }
