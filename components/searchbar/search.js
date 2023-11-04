175266cf1f59058ac40dd51a1e833204594fea4d
NeoDitjoe <ditjoeneo03@gmail.com>
2023-11-04 09:42:24 +0200

    installed debounce

--- components/searchbar/search.js
+++ components/searchbar/search.js
@@ -1,21 +1,30 @@
 import style from 'styles/search.module.css'
-import { Button } from '../button/button'
+import { useRef } from 'react'
+import { debounce } from 'lodash'
 
 export default function Search(){
+
+    const inputRef = useRef()
+
+    function searchHandler(){
+        console.log(inputRef.current.value)
+    }
+
+    const debounceSearchHandler = debounce(searchHandler, 1500)
+
     return (
         <div>
             <form className={style.form}>
                 <div className={style.formDiv}>
-                    <label htmlFor='search'>Your Password</label>
+                    <label htmlFor='search'></label>
                     <input
                         type='text'
                         id='search'
                         required
+                        placeholder='Search'
+                        ref={inputRef}
+                        onChange={debounceSearchHandler}
                     />
-                </div>
-
-                <div className={style.actions}>
-                    <Button name='Find' />
                 </div>
             </form>
         </div>
