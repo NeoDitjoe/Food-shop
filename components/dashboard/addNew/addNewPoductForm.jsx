dd6ccdbef025b9032ab6fcead3f8bed45647f139
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-14 12:44:00 +0200

    moved the add new form

--- components/dashboard/addNew/addNewPoductForm.jsx
+++ components/dashboard/addNew/addNewPoductForm.jsx
@@ -1,4 +1,4 @@
-import AddToMenuForm from "@/Dashboard/AddToMenuForm";
+import AddToMenuForm from './AddToMenuForm';
 import style from 'styles/dashboard.module.css'
 import { FormsGrid } from "@/components/grid";
 import LinkButton, { Button } from "@/components/button/button";
@@ -7,7 +7,7 @@ import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 
-export default function Dashboard() {
+export default function AddNewProduct() {
 
 	const [showAddMenu, setShowAddMenu] = useState(false)
 	const [showSpecialsForm, setShowSpecialsForm] = useState(false)
@@ -28,9 +28,6 @@ export default function Dashboard() {
 			{
 				checkUser && checkUserEmail 
 				? <div>
-				<button className={style.button}>
-					<LinkButton link={'dashboard2090BoBo/orders'} text='View Orders' />
-				</button>
 
 				<div className={style.main}>
 
@@ -41,12 +38,6 @@ export default function Dashboard() {
 						addMenu={<AddToMenuForm collection='menu' />}
 					/> : ''}
 					<br />
-
-					<Button 
-            click={() => setShowSpecialsForm(!showSpecialsForm)} name={["Specials ", showSpecialsForm ? <SlArrowUp /> : <SlArrowDown />]} />
-					{showSpecialsForm ? <FormsGrid
-						addMenu={<AddToMenuForm collection='specials' />}
-					/> : ''}
 				</div>
 			</div> : <div>Please log in with as partner <Link href={'/'}>back</Link></div>
 			}
--- pages/dashboard2090BoBo/index.js
+++ pages/dashboard2090BoBo/index.js
@@ -1,13 +1,10 @@
-import Dashboard from "@/components/dashboard/dashboard";
 import { Fragment } from "react";
-
 
 export default function dashboard(){
 
-
 	return(
 		<Fragment>
-			<Dashboard />
+			'dashboard'
 		</Fragment>
 
 	)
